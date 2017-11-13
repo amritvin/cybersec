264479f98cb12f2589dd1952e8acfdb89333c84f
amritvin <c.amritanand@gmail.com>
2017-11-13 12:53:52 +0530

    desk

--- networks/scapy/tdns.py
+++ networks/scapy/tdns.py
@@ -12,7 +12,6 @@ import hashlib
 scapy.conf.iface = "lo"
 scapy.conf.L3socket = scapy.L3RawSocket
 sport = random.randint(1024,65535)
-
 # SYN
 ip=scapy.IP(src='127.0.0.1',dst='127.0.0.1')
 SYN=scapy.TCP(sport=sport,dport=8000,flags='S',seq=1000)
@@ -25,13 +24,18 @@ data=scapy.sr1(ip/ACK)
 l = data.getlayer(scapy.Raw).load
 l=l.strip()
 hashed=hashlib.sha256(l.encode('ascii')).hexdigest()
+print hashed
 #
+
+print "sending hash . . . . . . . . . . . "
 a =scapy.IP(dst='127.0.0.1') / scapy.TCP() / scapy.Raw(load=hashed)
 scapy.sendp(a)
-# ...
+#
+print"Recieving FIN . . . . . . . . . . . . "
 FIN=ip/scapy.TCP(sport=sport, dport=8000, flags="FA", seq=SYNACK.ack, ack=SYNACK.seq + 1)
 FINACK=scapy.sr1(FIN)
 FINACK.show()
+print"sending ACK . . . . . . . . . . . . . . "
 LASTACK=ip/scapy.TCP(sport=sport, dport=8000, flags="A", seq=FINACK.ack, ack=FINACK.seq + 1)
 scapy.send(LASTACK)
 LASTACK.show()
